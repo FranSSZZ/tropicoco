378b2d5a342e3dc69a668549574e9db89155e688
Mariama <merysalhi32@gmail.com>
2023-04-17 15:05:30 +0200

    nueva rama

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const shoppingButton = document.getElementById('shopping-button');
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('quantity');
 
-shoppingButton.addEventListener('click', () => {
-  window.location.href = 'index.html';
-});
 
-
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+  body.classList.remove('active');
+})
+let products = [
+  {
+    id: 1,
+    name: 'PRODUCT NAME 1',
+    image: '1.PNG',
+    price: 2.75
+  }
+];
   
